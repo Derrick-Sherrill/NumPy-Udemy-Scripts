be235c00f2d12643c16375ba6f737a11c8ea7c4c
Derrick Sherrill <36718839+Derrick-Sherrill@users.noreply.github.com>
2019-08-17 22:03:17 +0100

    Add comments

--- Topic 1 - Arrays/numpy-arrays-lesson.py
+++ Topic 1 - Arrays/numpy-arrays-lesson.py
@@ -1,11 +1,13 @@
 import numpy as np
 
+# Creation of a sample array 
 array_a = np.array([1,2,3,4,5])
 print(array_a)
 
+# We can add elements of multiple data types to python lists, not arrays
 print([1,2,3,4,5, 'derrick'])
 
-print(array_a.shape)
+print(array_a.shape) # View the shape of an array by the shape attribute
 
-array_b = np.array([[1,2,3,4,5],[1,2,3,4,5]])
+array_b = np.array([[1,2,3,4,5],[1,2,3,4,5]]) #Multidimensional arrays
 print(array_b.shape)
