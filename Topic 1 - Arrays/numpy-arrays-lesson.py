f02fd9306d4ae33e6c7885a929f5e3b557851115
Derrick Sherrill <36718839+Derrick-Sherrill@users.noreply.github.com>
2019-08-17 11:11:30 +0100

    Add NumPy Arrays Lesson

---
+++ Topic 1 - Arrays/numpy-arrays-lesson.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+array_a = np.array([1,2,3,4,5])
+print(array_a)
+
+print([1,2,3,4,5, 'derrick'])
+
+print(array_a.shape)
+
+array_b = np.array([[1,2,3,4,5],[1,2,3,4,5]])
+print(array_b.shape)
