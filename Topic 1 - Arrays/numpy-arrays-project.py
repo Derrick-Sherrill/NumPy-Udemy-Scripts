1b38189d7825fe6045db0daabc3c126882c1cc01
Derrick Sherrill <36718839+Derrick-Sherrill@users.noreply.github.com>
2019-08-17 11:22:33 +0100

    NumPy Array Creation Project Addition

---
+++ Topic 1 - Arrays/numpy-arrays-project.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# 3x3
+seq_a = [1,2,3]
+seq_b = [4,5,6]
+seq_c = [7,8,9]
+seq_d = [10.5,11.5,12.5]
+
+array_abc = np.array([seq_a, seq_b, seq_c, seq_d])
+print(array_abc)
+
+array_d = np.array([[1,2],[3,4],[5,6],[7,8],[9,10]])
